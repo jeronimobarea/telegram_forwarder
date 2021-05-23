f57b7ed7e26a6a84c39598e03ca01e25f7c05d79
Jero <35541010+jeronimobarea@users.noreply.github.com>
2021-05-23 13:17:24 +0200

    changes

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ from telethon import events, TelegramClient
 from src.constants import Constants
 from src.utils import Utils
 
-with TelegramClient(
-        Constants.CHAT_NAME.value,
-        Constants.API_ID.value,
-        Constants.API_HASH.value,
-) as client:
-    @client.on(events.NewMessage())
-    async def handler(event) -> None:
-        if not event.chat:
-            return
-        if Utils.chat_title_matches(event.chat.title):
-            event.message.text = Utils.format_message(event.chat.title, event.message.text)
-            await client.send_message(Constants.CHAT_NAME.value, event.message)
+
+def main():
+    with TelegramClient(
+            Constants.CHAT_NAME.value,
+            Constants.API_ID.value,
+            Constants.API_HASH.value,
+    ) as client:
+        @client.on(events.NewMessage())
+        async def handler(event) -> None:
+            if not event.chat:
+                return
+            if Utils.chat_title_matches(event.chat.title):
+                event.message.text = Utils.format_message(event.chat.title, event.message.text)
+                await client.send_message(Constants.CHAT_NAME.value, event.message)
+
+        client.run_until_disconnected()
 
 
-    client.run_until_disconnected()
+if __name__ == "__main__":
+    print(f"Forwarding to {Constants.CHAT_NAME.value}")
+    main()
--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,8 @@
 import os
 from enum import Enum
+from dotenv import load_dotenv
+
+load_dotenv()
 
 API_ID: str = os.getenv("API_ID")
 API_HASH: str = os.getenv("API_HASH")
